bf8992825c4b94d24a6bbec12f18b7e08bbab2aa
jbugel-lol <jonasbugelnig+gh1@jbugel.xyz>
2024-04-21 21:22:40 +0200

    tailwindcss installed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header class="bg-black">
+    <h1 class="text-center text-white">Disflux avaible soon</h1>
+</header>
