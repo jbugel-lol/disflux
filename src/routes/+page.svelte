129a31f23caf31e33a2ebe3ba1cf0971fbe3d119
jbugel-lol <jonasbugelnig+gh1@jbugel.xyz>
2024-04-21 21:24:19 +0200

    style fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
-<header class="bg-black">
-    <h1 class="text-center text-white">Disflux avaible soon</h1>
+<header class="bg-black h-full w-full">
+	<h1 class="text-center text-white">Disflux avaible soon</h1>
 </header>
